a451e597f7ba6b25d003740cdd74b71c7f618072
Rodrigo Ferrarezi Figueiredo de Lima <rodrigofflima@hotmail.com>
2022-10-14 20:32:00 -0300

    feat: pokemons api loading

--- app/src/main/java/com/example/pokedex_android/api/PokemonRepository.kt
+++ app/src/main/java/com/example/pokedex_android/api/PokemonRepository.kt
@@ -1,4 +1,35 @@
 package com.example.pokedex_android.api
 
-class PokemonRepository {
+import android.util.Log
+import com.example.pokedex_android.api.model.PokemonApiResult
+import com.example.pokedex_android.api.model.PokemonsApiResult
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object PokemonRepository {
+    private val service: PokemonService
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        service = retrofit.create(PokemonService::class.java)
+    }
+
+    fun listPokemons(limit: Int = 151): PokemonsApiResult? {
+        var call = service.listPokemons(limit)
+
+        return call.execute().body()
+    }
+
+    fun getPokemon(number: Int): PokemonApiResult? {
+        var call = service.getPokemon(number)
+
+        return call.execute().body()
+    }
 }
--- app/src/main/java/com/example/pokedex_android/view/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex_android/view/PokemonAdapter.kt
@@ -6,10 +6,11 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.pokedex_android.R
 import com.example.pokedex_android.domain.Pokemon
 
-class PokemonAdapter(private val items: List<Pokemon>) :
+class PokemonAdapter(private val items: List<Pokemon?>) :
     RecyclerView.Adapter<PokemonAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,24 +28,26 @@ class PokemonAdapter(private val items: List<Pokemon>) :
     override fun getItemCount() = items.size
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindView(item: Pokemon) = with(itemView) {
+        fun bindView(item: Pokemon?) = with(itemView) {
             val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
             val tvNumber = findViewById<TextView>(R.id.tvNumber)
             val tvName = findViewById<TextView>(R.id.tvName)
             val tvType1 = findViewById<TextView>(R.id.tvType1)
             val tvType2 = findViewById<TextView>(R.id.tvType2)
 
-            //TODO: Load image with glide
+            item?.let{
+                Glide.with(itemView.context).load(it.imageUrl).into(ivPokemon)
 
-            tvNumber.text = "No ${item.formattNumber}"
-            tvName.text = item.name
-            tvType1.text = item.types[0].name
+                tvNumber.text = "No ${item.formattedNumber}"
+                tvName.text = item.formattedName
+                tvType1.text = item.types[0].name.capitalize()
 
-            if (item.types.size > 1) {
-                tvType2.visibility = View.VISIBLE
-                tvType2.text = item.types[1].name
-            } else {
-                tvType2.visibility = View.GONE
+                if (item.types.size > 1) {
+                    tvType2.visibility = View.VISIBLE
+                    tvType2.text = item.types[1].name.capitalize()
+                } else {
+                    tvType2.visibility = View.GONE
+                }
             }
         }
     }
